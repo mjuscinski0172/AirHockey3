893739bc70542e3d150934f4aa89a6240ef3abf2
student5 <student3@jh-mba158-13.d214.org>
2017-05-04 10:49:24 -0500

    made the entire app -om

--- AirHockey2/GameScene.swift
+++ AirHockey2/GameScene.swift
@@ -9,81 +9,117 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+
+let puckCategory: UInt32 = 0x1 << 0
+let bottomCategory: UInt32 = 0x1 << 1
+let topCategory: UInt32 = 0x1 << 2
+let leftCategory: UInt32 = 0x1 << 3
+let rightCategory: UInt32 = 0x1 << 4
+let paddleCategory: UInt32 = 0x1 << 5
+let leftGoalCategory: UInt32 = 0x1 << 6
+let rightGoalCategory: UInt32 = 0x1 << 7
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var rightPaddle = SKSpriteNode()
+    var leftPaddle = SKSpriteNode()
+    var puck = SKSpriteNode()
+    var rightGoal = SKSpriteNode()
+    var leftGoal = SKSpriteNode()
+    var leftScore = SKLabelNode()
+    var rightScore = SKLabelNode()
+    var leftScoreCounter = 0
+    var rightScoreCounter = 0
     
-    override func didMove(to view: SKView) {
+    override func didMove(to view: SKView)
+    {
+        print("Hello World")
+        rightPaddle = self.childNode(withName: "rightPaddle") as! SKSpriteNode
+        leftPaddle = self.childNode(withName: "leftPaddle") as! SKSpriteNode
+        puck = self.childNode(withName: "puck") as! SKSpriteNode
+        rightGoal = self.childNode(withName: "rightGoal") as! SKSpriteNode
+        leftGoal = self.childNode(withName: "leftGoal") as! SKSpriteNode
+        addChild(leftScore)
+        addChild(rightScore)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        physicsWorld.contactDelegate = self
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let bottomLeft = CGPoint(x: frame.origin.x, y: frame.origin.y)
+        let bottomRight = CGPoint(x: -frame.origin.x, y: frame.origin.y)
+        let topLeft = CGPoint(x: frame.origin.x, y: 284)
+        let topRight = CGPoint(x: -frame.origin.x, y: 284)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        let bottom = SKSpriteNode()
+        bottom.name = "bottom"
+        bottom.physicsBody = SKPhysicsBody(edgeFrom: bottomLeft, to: bottomRight)
+        
+        let left = SKSpriteNode()
+        left.name = "left"
+        left.physicsBody = SKPhysicsBody(edgeFrom: bottomLeft, to: topLeft)
+        
+        let top = SKSpriteNode()
+        top.name = "top"
+        top.physicsBody = SKPhysicsBody(edgeFrom: topLeft, to: topRight)
+        
+        let right = SKSpriteNode()
+        right.name = "right"
+        right.physicsBody = SKPhysicsBody(edgeFrom: topRight, to: bottomRight)
+        
+        addChild(bottom)
+        addChild(left)
+        addChild(top)
+        addChild(right)
+        
+        leftPaddle.physicsBody?.categoryBitMask = paddleCategory
+        rightPaddle.physicsBody?.categoryBitMask = paddleCategory
+        puck.physicsBody?.categoryBitMask = puckCategory
+        leftGoal.physicsBody?.categoryBitMask = leftGoalCategory
+        rightGoal.physicsBody?.categoryBitMask = rightGoalCategory
+        
+        puck.physicsBody?.contactTestBitMask = paddleCategory | leftGoalCategory | rightGoalCategory
+        
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let location = touch.location(in: self)
+            if location.x > 0 && location.y < 249{
+                rightPaddle.run(SKAction.move(to: location, duration: 0.1))
+            }
+            if location.x < 0 && location.y < 249 {
+                leftPaddle.run(SKAction.move(to: location, duration: 0.1))
+            }
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            let location = touch.location(in: self)
+            if location.x > 0 && location.y < 249{
+                rightPaddle.run(SKAction.move(to: location, duration: 0.1))
+            }
+            if location.x < 0 && location.y < 249 {
+                leftPaddle.run(SKAction.move(to: location, duration: 0.1))
+            }
+        }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.categoryBitMask == rightGoalCategory {
+            print("Here")
+            leftScoreCounter += 1
+            print(leftScoreCounter)
+            leftScore.text = "\(leftScoreCounter)"
+        }
+        else if contact.bodyA.categoryBitMask == leftGoalCategory {
+            rightScoreCounter += 1
+            rightScore.text = "\(rightScoreCounter)"
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
     }
 }

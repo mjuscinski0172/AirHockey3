31168753839fe6c19b9ba5f64935efbce0159cd9
student4 <student4@jh-mba158-10.d214.org>
2017-05-05 09:53:00 -0500

    fixed puck resetting to blue line before to center after win

--- AirHockey2/GameScene.swift
+++ AirHockey2/GameScene.swift
@@ -111,12 +111,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if contact.bodyA.categoryBitMask == rightGoalCategory {
             leftScoreCounter += 1
             leftScore.text = "\(leftScoreCounter)"
-            puck.run(SKAction.move(to: CGPoint(x: 150, y: -50), duration: 0.0))
+            if leftScoreCounter == 10 {
+                winnerLabel.text = "Player 1 Wins!"
+                reset()
+            }
+            else {
+                puck.run(SKAction.move(to: CGPoint(x: 150, y: -50), duration: 0.0))
+            }
         }
         else if contact.bodyA.categoryBitMask == leftGoalCategory {
             rightScoreCounter += 1
             rightScore.text = "\(rightScoreCounter)"
-            puck.run(SKAction.move(to: CGPoint(x: -150, y: -50), duration: 0.0))
+            if rightScoreCounter == 10 {
+                winnerLabel.text = "Player 2 Wins!"
+                reset()
+            }
+            else {
+                puck.run(SKAction.move(to: CGPoint(x: -150, y: -50), duration: 0.0))
+            }
+        }
+    }
+    
+    func reset() {
+        let delayInSeconds = 4.0
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delayInSeconds) {
+            self.winnerLabel.text = "Play!"
+            self.leftScore.text = "0"
+            self.rightScore.text = "0"
+            self.leftScoreCounter = 0
+            self.rightScoreCounter = 0
+            let puckPoint = CGPoint(x: 0, y: -50)
+            let rightPaddlePoint = CGPoint(x: 410, y: -50)
+            let leftPaddlePoint = CGPoint(x: -410, y: -50)
+            self.puck.run(SKAction.move(to: puckPoint, duration: 0))
+            self.rightPaddle.run(SKAction.move(to: rightPaddlePoint, duration: 0))
+            self.leftPaddle.run(SKAction.move(to: leftPaddlePoint, duration: 0))
+
+            
         }
     }
     
